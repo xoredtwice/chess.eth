d1738440bda0ae7fac629daefddf2459af0f2329
Mehdi <xoredtwice@pm.me>
2022-07-01 20:13:30 -0400

    Added a simple view for easier testing.

--- src/view/table_view.py
+++ src/view/table_view.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QDialog, QPushButton, QLineEdit
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QLabel
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 from src.utils.logger import lprint, lsection
 
-# 
+from src.pychess.chess_utils import load_game_state, parse_board, print_board
+
 class TableView(QDialog):
 
     # constructor
@@ -16,21 +18,45 @@ class TableView(QDialog):
         
         self.refreshButton = QPushButton('Refresh')
         self.refreshButton.setFixedWidth(300)
+        self.refreshButton.setFixedHeight(100)
         self.refreshButton.clicked.connect(self.onClicked_refresh)
 
+        self.squares = []
         # Constructing the layout, adding the components
         main_layout = QVBoxLayout()
 
         # l2 is a Horizontal layout. Custom components are here
-        h1_layout = QHBoxLayout()
+        chessLayout = QHBoxLayout()
+        xOffset = 10
+        yOffset = 10
+        sqSize = 50
+        for i in range(8):
+            file_layout = QVBoxLayout()
+            for j in range(8):
+                newSq = QLabel(' ')
+                newSq.resize(sqSize, sqSize)
+                newSq.move(xOffset + i * sqSize, yOffset + j * sqSize)
+                newSq.setAlignment(QtCore.Qt.AlignCenter)
+                if (i+j) % 2 == 0:
+                    newSq.setStyleSheet("QLabel { background-color : purple; color : black; font-size: 40pt; }");
+                else:
+                    newSq.setStyleSheet("QLabel { background-color : white; color : black; font-size: 40pt;}");
+                file_layout.addWidget(newSq)
+                self.squares.append(newSq)
+            chessLayout.addLayout(file_layout)
 
         l2_v1_layout = QVBoxLayout()
         l2_v1_layout.addWidget(self.refreshButton)
-        h1_layout.addLayout(l2_v1_layout)
 
         # ----------------------------------------------------
-        main_layout.addLayout(h1_layout)
+        main_layout.addLayout(chessLayout)
+        main_layout.addLayout(l2_v1_layout)
 
         self.setLayout(main_layout)
     def onClicked_refresh(self):
-        lprint("Not implemented")
+        turn, board64W, board64B, pieces256, engagements, visibility = load_game_state()
+        pieces, view = parse_board(pieces256)
+        for rank in range(8):
+            for file in range(8):
+                self.squares[file * 8 + (7-rank)].setText(view[rank][file])
+        print_board(view)
