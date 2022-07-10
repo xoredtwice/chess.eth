987df94fe8c95364280c5717ac2892cbd8299aa0
Mehdi <xoredtwice@pm.me>
2022-07-10 03:45:24 -0400

    A simple three move game has been added to test checkmate

--- test/sample_match.sh
+++ test/sample_match.sh
@@ -2,5 +2,18 @@
 
 
 python test_pychess.py -c move -p W_P_E -s E4
+python test_pychess.py -c move -p B_P_E -s E5
 
+python test_pychess.py -c move -p W_Q -s H5
+python test_pychess.py -c move -p B_P_A -s A6
+
+python test_pychess.py -c move -p W_B_F -s C4
+python test_pychess.py -c move -p B_P_B -s B6
+
+# checkmate
+python test_pychess.py -c move -p W_Q -s F7
+
+# checking post checkmate move
+python test_pychess.py -c move -p B_P_A -s A5
+python test_pychess.py -c move -p B_K -s E7
 
